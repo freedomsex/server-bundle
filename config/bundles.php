f409155ef9d84f3c6c0fe5ffb849eed78684dd20
tebaly <support@freedomsex.net>
2021-12-15 18:37:25 +0400

    fix: upd

--- config/bundles.php
+++ config/bundles.php
@@ -1,13 +1,8 @@
 <?php
 
 return [
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     FreedomSex\HealthBundle\HealthBundle::class => ['all' => true],
-    FreedomSex\AuthBundle\AuthBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
